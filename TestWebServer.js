bad7f4ba6e507cea71eeb971039ca37146248436
cuong12348 <129239071+cuong12348@users.noreply.github.com>
2023-09-18 13:59:58 +0700

    TestWebServer

---
+++ TestWebServer.js
@@ -0,0 +1,10 @@
+const express = require ('express')
+const app = express()
+
+app.get('/',(req,res)=> {
+    res.send('Hello,world!')
+})
+
+app.listen(300,()=>{
+    console.log('Server started on port 3000')
+})
